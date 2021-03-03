edf32716d4988b6838d061ade1d699219754b8e6
Renato da Costa Castelo <rccastelo@gmail.com>
2021-03-03 20:20:39 -0300

    Refatoracao dos projetos

--- rcLogApiNetCore/Controllers/LogController.cs
+++ rcLogApiNetCore/Controllers/LogController.cs
@@ -27,7 +27,36 @@ namespace rcLogApiNetCore.Controllers
                 logModel = null;
             }
 
-            logRetorno.TratarLinks();
+            //logRetorno.TratarLinks();
+
+            if (logRetorno.Erro || !logRetorno.Validacao) {
+                return BadRequest(logRetorno);
+            } else {
+                return Ok(logRetorno);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Incluir(LogTransfer logDados)
+        {
+            LogModel logModel;
+            LogTransfer logRetorno;
+
+            try {
+                logModel = new LogModel();
+
+                logRetorno = logModel.Incluir(logDados);
+            } catch (Exception ex) {
+                logRetorno = new LogTransfer();
+
+                logRetorno.Validacao = false;
+                logRetorno.Erro = true;
+                logRetorno.IncluirMensagem("Erro em LogController Incluir [" + ex.Message + "]");
+            } finally {
+                logModel = null;
+            }
+
+            // log.TratarLinks();
 
             if (logRetorno.Erro || !logRetorno.Validacao) {
                 return BadRequest(logRetorno);
--- rcLogApiNetCore/Models/LogModel.cs
+++ rcLogApiNetCore/Models/LogModel.cs
@@ -56,5 +56,42 @@ namespace rcLogApiNetCore.Models
 
             return logRetorno;
         }        
+
+        public LogTransfer Incluir(LogTransfer logDados)
+        {
+            LogDataModel logDataModel;
+            LogTransfer logValidacao;
+            LogTransfer logRetorno;
+
+            try {
+                // logBusiness = new LogBusiness();
+                logDataModel = new LogDataModel();
+
+                // logValidacao = logBusiness.Validar(pLog);
+                logValidacao = new LogTransfer(logDados);
+
+                if (!logValidacao.Erro) {
+                    if (logValidacao.Validacao) {
+                        logRetorno = logDataModel.Incluir(logValidacao);
+                    } else {
+                        logRetorno = new LogTransfer(logValidacao);
+                    }
+                } else {
+                    logRetorno = new LogTransfer(logValidacao);
+                }
+            } catch (Exception ex) {
+                logRetorno = new LogTransfer();
+
+                logRetorno.Validacao = false;
+                logRetorno.Erro = true;
+                logRetorno.IncluirMensagem("Erro em LogModel Incluir [" + ex.Message + "]");
+            } finally {
+                logDataModel = null;
+                // logBusiness = null;
+                logValidacao = null;
+            }
+
+            return logRetorno;
+        }
     }
 }
